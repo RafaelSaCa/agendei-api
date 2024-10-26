3e9aaa2ad376ed0692b8fefc344209c28f3cbd70
Rafael Saca <raffaellsaca@gmail.com>
2024-10-25 23:53:15 -0300

    feat: Routes

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import controllerDoctor from "./controllers/controller.doctor.js"
+
+const router = Router();
+
+router.get("/doctors", controllerDoctor.Listar);
+
+
+
+export default router;
