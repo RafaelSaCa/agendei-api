cc3d50b35f495776a3b07a6ab95c3d9a799b56fe
Rafael Saca <raffaellsaca@gmail.com>
2024-10-28 22:44:49 -0300

    feat: Controller CRUD

--- src/controllers/controller.doctor.js
+++ src/controllers/controller.doctor.js
@@ -8,4 +8,37 @@ async function Listar(req, res) {
   res.status(200).json(doctors);
 }
 
-export default { Listar };
+async function Inserir(req, res) {
+/*
+  const name = req.body.name;
+  const specialty = req.body.specialty;
+  const icon = req.body.icon;
+*/
+  const {name, specialty, icon} = req.body;
+
+  const doctor = await serviceDoctor.Inserir(name,specialty,icon);
+
+  res.status(201).json(doctor);
+}
+
+async function Editar(req, res) {
+
+  const id_doctor = req.params.id_doctor;
+  const {name, specialty, icon} = req.body;
+
+  const doctor = await serviceDoctor.Editar(id_doctor, name,specialty,icon);
+
+  res.status(200).json(doctor);
+}
+
+async function Excluir(req, res) {
+
+  const id_doctor = req.params.id_doctor;
+
+  const doctor = await serviceDoctor.Excluir(id_doctor);
+
+  res.status(200).json(doctor);
+}
+
+
+export default { Listar, Inserir, Editar, Excluir};
