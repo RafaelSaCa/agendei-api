b693b75accb78d1fbf7d35ecc70bd089e699f0f1
Rafael Saca <raffaellsaca@gmail.com>
2024-10-28 22:45:25 -0300

    feat: Service CRUD

--- src/services/service.doctor.js
+++ src/services/service.doctor.js
@@ -7,4 +7,27 @@ async function Listar(name) {
   return doctors;
 }
 
-export default { Listar };
+async function Inserir(name,specialty,icon) {
+  
+  const doctor = await repositoryDoctor.Inserir(name,specialty,icon);
+  
+  return doctor;
+}
+
+async function Editar(id_doctor, name,specialty,icon) {
+  
+  const doctor = await repositoryDoctor.Editar(id_doctor, name,specialty,icon);
+  
+  return doctor;
+}
+
+
+async function Excluir(id_doctor) {
+  
+  const doctor = await repositoryDoctor.Excluir(id_doctor);
+  
+  return doctor;
+}
+
+
+export default { Listar, Inserir, Editar, Excluir};
