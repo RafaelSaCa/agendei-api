e75e1316d36cebbff991b84a4786f353f3bfdfc7
Rafael Saca <raffaellsaca@gmail.com>
2024-10-28 21:38:46 -0300

    feat: Busca por nome

--- src/repositories/respository.doctor.js
+++ src/repositories/respository.doctor.js
@@ -1,10 +1,20 @@
 import { query } from "../database/sqlite.js";
 
 
-async function Listar() {
-  let sql = "select * from doctors order by name";
+async function Listar(name) {
 
-  const doctors = await query(sql, []);
+  let filtro = [];
+
+  let sql = "select * from doctors ";
+
+  if (name){
+     sql = sql + "where name like ? ";
+     filtro.push('%' + name + '%');
+  }
+
+  sql= sql + "order by name"
+
+  const doctors = await query(sql,filtro);
   
   return doctors;
 }
